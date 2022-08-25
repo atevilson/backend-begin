cc4650de81111272eced9d08cf8a7f50a399d8d0
atevilson <freitas.atevilson@gmail.com>
2022-08-24 22:48:12 -0300

    add tipo de dados string

---
+++ dataTypes/type-string.js
@@ -0,0 +1,5 @@
+const string1 = "isso é apenas um exemplo de tipo de dados";
+
+const string2 = "será que isso vai me tornar um engenheiro de software ?";
+
+console.log(`Olá, ${string1}, mas ${string2}`);
