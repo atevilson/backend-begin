0de912a861a6dec1d8445b2ce85718e971f4209a
atevilson <freitas.atevilson@gmail.com>
2022-08-24 22:42:06 -0300

    add tipo booleano e numero

---
+++ dataTypes/type-boolean.js
@@ -0,0 +1,5 @@
+const boolean1 = 18;
+
+const boolean2 = 21;
+
+console.log(boolean1 <= boolean2);
---
+++ dataTypes/type-number.js
@@ -0,0 +1,7 @@
+// Number and float
+
+const numberOne = 1.22
+
+const numberTwo = 3.12
+
+console.log((numberOne + numberTwo) / 2);
