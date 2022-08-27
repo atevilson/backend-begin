59468cb97d8a757c2e965c3a9a89b9ac532fb122
atevilson <freitas.atevilson@gmail.com>
2022-08-27 00:42:39 -0300

    add tipos de dados verdadeiros, falsos e nulo

---
+++ dataTypes/truthy-falsy.js
@@ -0,0 +1,12 @@
+/*
+console.log(null == false);
+console.log(undefined == false);
+console.log(0 == false);
+console.log("" == false); */
+
+let number;
+let text = null;
+
+console.log(typeof number);
+
+console.log(typeof text); // Null é um tipo de dado que é um objeto no javaScript
