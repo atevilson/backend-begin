1bb4cd79f17633bc8f0cf2d1e9d19a7a47858bec
atevilson <freitas.atevilson@gmail.com>
2022-08-27 01:00:41 -0300

    add como fazer conversões entre strings e number

---
+++ dataTypes/conversao.js
@@ -0,0 +1,15 @@
+// Conversão implícita
+
+const number1 = "18";
+
+const number2 = 18;
+
+console.log(number1 + number2); // resultado = 1818
+
+// Conversão explícita
+
+const number3 = "18";
+
+const number4 = 18;
+
+console.log(Number(number1) + number2); // resultado = 36
