5565b3632a120264d54cf13263d97a9f16b36d68
atevilson <freitas.atevilson@gmail.com>
2022-09-07 20:48:53 -0300

    add módulo função teste de gravidez

---
+++ js-funcoes/func.js
@@ -0,0 +1,10 @@
+// Função teste de gravidez
+
+function child() {
+    let mother = "Carmen";
+    let father = "Atevilson";
+    const makeLove = mother !== father ? "Erick Matheus" : "Alarme falso"; 
+    console.log(makeLove);
+};
+
+child();
