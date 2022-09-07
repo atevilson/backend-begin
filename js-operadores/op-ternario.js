7bdad097b3f08c9df13b7198113f96738442b130
atevilson <freitas.atevilson@gmail.com>
2022-09-07 19:12:47 -0300

    add operador terrnário

---
+++ js-operadores/op-ternario.js
@@ -0,0 +1,10 @@
+// Condicional com operador ternário
+
+const idadeMinima  = 18;
+const idadeCliente = 15;
+
+const result = idadeCliente >= idadeMinima ? "Pode vender cerveja!" : "Não pode vender cerveja!" 
+
+// console.log(idadeCliente >= idadeMinima   ? "Pode vender" : "Não pode vender"); 
+
+console.log(result);
