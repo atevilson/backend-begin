25886cdd6e49496822e3bfcd8f675f396b48280a
atevilson <freitas.atevilson@gmail.com>
2022-09-07 13:54:47 -0300

    add comparações implicitas e explicitas

---
+++ js-operadores/comparacoes.js
@@ -0,0 +1,9 @@
+// (2x=) '==' (Comparação implícita, só compara o valor)
+
+const digito = 10;
+const texto = "10";
+
+console.log(digito == texto);
+
+// (3x=) '===' (Comparação explícita, compara o valor e o tipo de dado)
+console.log(digito === texto);
