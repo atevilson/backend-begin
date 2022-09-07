4822202870023502d61f712fc0a01a42cf934f2f
atevilson <freitas.atevilson@gmail.com>
2022-09-07 13:55:22 -0300

    update

--- js-operadores/comparacoes.js
+++ js-operadores/comparacoes.js
@@ -1,9 +1,9 @@
-// (2x=) '==' (Comparação implícita, só compara o valor)
+// (2x=) '==' (Comparação implicita, só compara o valor)
 
 const digito = 10;
 const texto = "10";
 
 console.log(digito == texto);
 
-// (3x=) '===' (Comparação explícita, compara o valor e o tipo de dado)
+// (3x=) '===' (Comparação explicita, compara o valor e o tipo de dado)
 console.log(digito === texto);
