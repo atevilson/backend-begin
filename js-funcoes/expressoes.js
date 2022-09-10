5fd494ecbfe4459ff8e9e699fd59020ffafc62ac
atevilson <freitas.atevilson@gmail.com>
2022-09-09 21:26:47 -0300

    funçoes declaradas com variáveis

---
+++ js-funcoes/expressoes.js
@@ -0,0 +1,7 @@
+// Expressões de função, declarando com variáveis.
+
+const divisao = function(a,b) { return a / b };
+
+const multiplica = function(c,d) { return c * d };
+
+console.log(multiplica(divisao(10,2), divisao(10,2)));
