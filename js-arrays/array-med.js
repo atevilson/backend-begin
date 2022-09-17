0e18bf5331f1f73190c60edea2715f729d54190f
atevilson <freitas.atevilson@gmail.com>
2022-09-17 20:28:41 -0300

    add função para deletar um item do array

--- js-arrays/array-med.js
+++ js-arrays/array-med.js
@@ -5,6 +5,9 @@ let notas = [10, 6.5, 8, 7.5];
 // adicionando uma nota
 notas.push(5.5);
 
+//deletando nota na posição 4 do array
+notas.splice(4, 4);
+
 let result = notas.reduce((soma, atual) => soma + atual) / notas.length;
 
-console.log(result);
+console.log(`A média das notas é ${result}`);
