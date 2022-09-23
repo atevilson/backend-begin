aec3fd0361a38c64c8b5c5e30eb5eeb37b17b1c6
atevilson <freitas.atevilson@gmail.com>
2022-09-23 20:50:56 -0300

    add concatenação de arrays

---
+++ js-arrays/array-family.js
@@ -0,0 +1,7 @@
+const familia1 = ["Mica", "Puca", "Panda", "Lion"];
+
+const familia2 = ["Mulan", "Miler", "Mah", "Jump"];
+
+const fusaoFamilia = familia1.concat(familia2);
+
+console.log(fusaoFamilia);
