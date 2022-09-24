a6700f8ee7750cd804e7b27bf9f723f8aeff6078
atevilson <freitas.atevilson@gmail.com>
2022-09-23 21:20:55 -0300

    add unificação de arrays

---
+++ js-arrays/array-desejo.js
@@ -0,0 +1,10 @@
+const maes = ["Carmen", "Diana", "Daiane", "Milena"];
+
+const desejos = ["Maça", "Uva", "Jaca", "Muruci"];
+
+const desejosDeMaes = [maes, desejos];
+
+console.log(`O desejo da mamãe ${desejosDeMaes[0][0]} é comer ${desejosDeMaes[1][2]}`);
+console.log(`O desejo da mamãe ${desejosDeMaes[0][1]} é comer ${desejosDeMaes[1][1]}`);
+console.log(`O desejo da mamãe ${desejosDeMaes[0][3]} é comer ${desejosDeMaes[1][0]}`);
+console.log(`O desejo da mamãe ${desejosDeMaes[0][2]} é comer ${desejosDeMaes[1][3]}`);
